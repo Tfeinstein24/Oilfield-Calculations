d0c564f9361c0c307f9cd99fb8cf7397252b194c
Tfeinstein24 <taylorsfeinstein@gmail.com>
2017-04-28 14:47:25 -0500

    Add files via upload

---
+++ satellite.js
@@ -0,0 +1,20 @@
+
+ 
+function showWeatherInfo() {
+    var queryURL = "https://api.wunderground.com/api/cb39f281310e1f9d/satellite/q/TX/Houston.json";
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        console.log(response);
+        var pictureSRC = response.satellite.image_url_vis;
+        var image = $("<img id='satellite'>")
+        image.attr("src", pictureSRC);
+        $("#satellite").append(image);
+
+    })
+}
+
+showWeatherInfo();
