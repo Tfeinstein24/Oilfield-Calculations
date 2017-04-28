911446fd447835a64a3e71656004cc402ef24f6b
Tfeinstein24 <taylorsfeinstein@gmail.com>
2017-04-27 20:24:56 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 // This is our API key
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
     // Here we are building the URL we need to query the database
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
       "q=Houston,Texas&units=imperial&appid=" + APIKey;
 
     // Here we run our AJAX call to the OpenWeatherMap API
