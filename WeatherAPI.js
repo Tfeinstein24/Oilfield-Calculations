45f41111495dc1c1010a1ac1043cf1739909d93b
Tfeinstein24 <taylorsfeinstein@gmail.com>
2017-04-28 12:45:03 -0500

    Add files via upload

---
+++ WeatherAPI.js
@@ -0,0 +1,14 @@
+function showWeatherInfo() {
+    var queryURL = "https://api.wunderground.com/api/cb39f281310e1f9d/conditions/q/TX/Houston.json";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        console.log(response);
+        $("#weather").html("Houston Temp: " + response.current_observation.temp_f + "<br>" + "Current Wind Speed: " + response.current_observation.wind_mph + "<br>" + "Wind Direction: " + response.current_observation.wind_dir + "<br>" + "<img src='http://icons.wxug.com/i/c/k/mostlycloudy.gif'>");
+
+    })
+}
+
+showWeatherInfo();
